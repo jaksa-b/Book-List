04130cc62382cdac88b0cc10bcb0e6f62a8b142b
Jakša Bašić <jaksabasic1@gmail.com>
2015-09-11 13:31:13 +0200

    Add New Books Form

--- client/bookList.js
+++ client/bookList.js
@@ -2,8 +2,14 @@
  * Created by Jakša Bašić on 11/09/15.
  */
 
-Template.booklist.helpers({
+Template.bookList.helpers({
     books: function () {
         return Books.find();
+    }
+});
+Template.bookList.events({
+    "submit .add-book": function(event) {
+        event.preventDefault(); // this prevents built-in form submission
+        Meteor.call('addBook', {title: event.target.title.value, author: event.target.author.value})
     }
 });
