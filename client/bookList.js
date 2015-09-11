5f373004411ec727684d957112cc67868cf73afa
Jakša Bašić <jaksabasic1@gmail.com>
2015-09-11 14:44:37 +0200

    added navbar and bootstrap3

--- client/bookList.js
+++ client/bookList.js
@@ -10,6 +10,8 @@ Template.bookList.helpers({
 Template.bookList.events({
     "submit .add-book": function(event) {
         event.preventDefault(); // this prevents built-in form submission
-        Meteor.call('addBook', {title: event.target.title.value, author: event.target.author.value})
+        Meteor.call('addBook', {title: event.target.title.value, author: event.target.author.value});
+        event.target.title.value = '';
+        event.target.author.value = '';
     }
 });
