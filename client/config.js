5928688ea5085418b9ec4c51465d0f911cacab41
Jakša Bašić <jaksabasic1@gmail.com>
2015-09-11 14:04:26 +0200

    User Authentication

---
+++ client/config.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Jakša Bašić on 11/09/15.
+ */
+Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+});
--- server/publications.js
+++ server/publications.js
@@ -2,5 +2,5 @@
  * Created by Jakša Bašić on 11/09/15.
  */
 Meteor.publish('books', function () {
-    return Books.find({});
+    return Books.find({userID: this.userId});
 });
